93827c3e723faaa045cc31db65f672032f92f38c
samarpan das <mailme.samarpandas@gmail.com>
2020-11-20 12:45:49 +0530

    content.js problem still unsolved

--- content.js
+++ content.js
@@ -8,4 +8,17 @@ function gotMessage(message, sender, sendResponse)
 
 }
 
-console.log(document.getElementsByClassName("lziZub tLDEHd"));
+
+// start of code that looks for files with links in the preview mode 
+var className = 'VkhHKd e7EEH nQaZq';  // unique for each a/c
+var links = document.getElementsByClassName(className), hrefs = [];
+//console.log(links);
+for (var i = 0; i < links.length; i++)
+{   
+    hrefs.push(links[i].href);
+}
+
+console.log(hrefs);
+console.log(links.length);
+
+// end of code that looks for files with links in the preview mode 
