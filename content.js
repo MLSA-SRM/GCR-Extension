350a1a416221347ac34355f2522d36e450a36392
samarpan das <mailme.samarpandas@gmail.com>
2020-11-20 14:26:28 +0530

    preview links successfully retrieved
    The code now is able to look for links of files that are present in the preview mode and is successfully able to capture them in the "href" array.

--- content.js
+++ content.js
@@ -5,20 +5,25 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendResponse)
 {
     console.log(message.txt);
-
 }
 
 
 // start of code that looks for files with links in the preview mode 
-var className = 'VkhHKd e7EEH nQaZq';  // unique for each a/c
-var links = document.getElementsByClassName(className), hrefs = [];
-//console.log(links);
-for (var i = 0; i < links.length; i++)
-{   
-    hrefs.push(links[i].href);
-}
 
-console.log(hrefs);
-console.log(links.length);
+function getPreviewLink()
+{
+    var className = 'VkhHKd e7EEH nQaZq';  // unique for each a/c
+    var links = document.getElementsByClassName(className), hrefs = [];
+    
+    for (var i = 0; i < links.length; i++)
+    {   
+        hrefs.push(links[i].href);
+    }
+
+    console.log(hrefs);
+    console.log(links.length);
+}
+setTimeout(getPreviewLink, 7000); 
+                // the time can be modified. 7 secs seems to be optimal.
 
 // end of code that looks for files with links in the preview mode 
